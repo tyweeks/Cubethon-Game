496107b56a3341526e30715fcb06bf10ae987cd4
Ty Weeks <ty.weeks10@gmail.com>
2022-09-28 21:03:21 -0700

    Added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,23 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
     void FixedUpdate()
     {
-        rb.AddForce(0f, 0f, 2000f * Time.deltaTime);
+        rb.AddForce(0f, 0f, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
     }
 }
