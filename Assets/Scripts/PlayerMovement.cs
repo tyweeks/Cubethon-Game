15fed676ba1588a9fc24dd1867dfcac4d9f2b597
Ty Weeks <ty.weeks10@gmail.com>
2022-10-03 15:08:56 -0700

    Added the ability to jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,48 @@ public class PlayerMovement : MonoBehaviour
 
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
+    public float jumpForce = 5f;
+    public float jumpRotationForce = 1.1f;
+
+    private bool playerShouldJump = false;
+    private bool playerCanMove = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerCanMove)
+        {
+            playerShouldJump = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            playerCanMove = true;
+        }
+    }
 
     void FixedUpdate()
     {
         rb.AddForce(0f, 0f, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") && playerCanMove)
         {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a") && playerCanMove)
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (playerShouldJump)
+        {
+            rb.AddForce(0, jumpForce, 0, ForceMode.VelocityChange);
+            rb.AddTorque(jumpRotationForce, 0, 0, ForceMode.VelocityChange);
+            playerCanMove = false;
+            playerShouldJump = false;
         }
 
         if (rb.position.y < -1)
